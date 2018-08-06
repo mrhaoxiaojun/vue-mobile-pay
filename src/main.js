1889443b382da8738a8dc2a1dd7e01dea2f3ffbc
haoxiaojun <1458149969@qq.com>
2018-08-06 19:43:08 +0800

    修改引入

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuePayPwd from './lib/index'
-// import vuePayPwd from '../dist/vue-pay-pwd'
+// import vuePayPwd from '../dist/vue-mobile-pay'
 Vue.use(vuePayPwd)
 new Vue({
   el: '#app',
